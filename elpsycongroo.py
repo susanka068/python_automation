ddd4c451dd63857b44a438c179c0eea664164863
susanka majumder <40341077+susanka068@users.noreply.github.com>
2019-11-01 18:52:34 +0530

    implemented user interactivity

--- elpsycongroo.py
+++ elpsycongroo.py
@@ -3,12 +3,9 @@ from uiautomator import Device
 from time import sleep
 import os
 im = ['images(8).jpeg']
-def crowdsource():
-  print("Inside Call Method.")
-  #image_capture = d(className='android.widget.TextView',
-                   #  packageName='com.google.android.apps.village.boond',
-                       # resourceId='com.google.android.apps.village.boond:id/tasktype_name',
-                       # text = 'Image Capture').click()
+def crowdsource(s):
+  print("Process no.")
+
 #upload
   #sleep(1)
   upload =  d(className='android.widget.ImageButton',
@@ -31,34 +28,26 @@ def crowdsource():
   tick =  d(className='android.view.View',
                      packageName='com.android.camera',
                         resourceId='com.android.camera:id/save_btn').click.wait()
- #goto selected file
- # selectedfile = d(className='android.widget.TextView',
-               #      packageName='com.android.documentsui',
-                  #      resourceId='android:id/title',
-              #          text = 'Download').click()
-
-#open 
- # selectedfile = d(className='android.widget.TextView',
-                 #    packageName='com.android.documentsui',
-                     #   resourceId='com.android.documentsui:id/date',
-                    #    text = 'Oct 27' ).click()
+ 
 
   #write
   write = d(className='android.widget.EditText',
                      packageName='com.google.android.apps.village.boond',
                         resourceId='com.google.android.apps.village.boond:id/edit_text_input' , 
-                        text =' Label key elements using commas, e.g., plant, vine, jasmine ').set_text("room-")
+                        text =' Label key elements using commas, e.g., plant, vine, jasmine ').set_text(s)
  #submit
-  sleep(2)
+  sleep(3)
   submit = d(className='android.widget.Button',
                      packageName='com.google.android.apps.village.boond',
                         resourceId='com.google.android.apps.village.boond:id/submit_button',
                         text = 'Upload' ).click()
 #Call The main Method
-n = 8000
+n = int(input("Enter How many times you want to repeat the process:"))
+s = input("Enter your default caption (e.g - your current location): ")
 for i in range(n):
-    crowdsource()
-    print(i)
+    crowdsource(s)
+    print(i+1)
+   
 
-print("Your test is completed...")
+print(" Voila ! Your Upload is completed...")
 
